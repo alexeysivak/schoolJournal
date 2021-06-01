79dbe012110a62c141f10dcb38334672ced3f4b7
AlSivak <olexiysyvak@gmail.com>
2021-06-01 22:51:05 +0300

    init: app creation; created webpack config file, files to test webpack configs. created html file with markup for app. created less file with styles for app; created js file for test.

---
+++ src/js/main.js
@@ -0,0 +1,3 @@
+import '../styles/main.less';
+
+console.log('app works corectly');
---
+++ webpack.config.js
@@ -0,0 +1,80 @@
+const path = require('path');
+const HTMLWebpackPlugin = require('html-webpack-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const TerserWebpackPlugin = require('terser-webpack-plugin');
+const CssMinimizerPlugin = require('css-minimizer-webpack-plugin');
+
+const isDev = process.env.NODE_ENV === 'development';
+const isProd = !isDev;
+
+function getOptimizationSettings() {
+	const settings = {
+		minimizer: [],
+	};
+
+	if (isProd) {
+		settings.minimizer = [
+			new TerserWebpackPlugin(),
+			new CssMinimizerPlugin(),
+		];
+	}
+
+	return settings;
+}
+
+module.exports = {
+	entry: ['@babel/polyfill', './src/js/main.js'],
+	output: {
+		path: path.resolve(__dirname, 'dist'),
+		filename: 'bundle.[contenthash].js',
+	},
+
+	resolve: {
+		extensions: ['.js', '.json'],
+	},
+
+	optimization: getOptimizationSettings(),
+
+	plugins: [
+		new HTMLWebpackPlugin({
+			template: path.resolve(__dirname, 'src/index.html'),
+			minify: {
+				collapseWhitespace: isProd,
+			},
+		}),
+		new CleanWebpackPlugin(),
+		new MiniCssExtractPlugin({
+			filename: 'main.[contenthash].css',
+		}),
+	],
+
+	devServer: {
+		port: 4200,
+	},
+
+	module: {
+		rules: [
+			{
+				test: /\.less$/,
+				use: [
+					{
+						loader: MiniCssExtractPlugin.loader,
+					},
+					'css-loader',
+					'less-loader',
+				],
+			},
+			{
+				test: /\.js$/,
+				exclude: /(node_modules|bower_components)/,
+				use: {
+					loader: 'babel-loader',
+					options: {
+						presets: ['@babel/preset-env'],
+					},
+				},
+			},
+		],
+	},
+};
